fcade79d630d3cffaa304b74bb1d5a49e9f2e5db
Antoine SPANIER <antoine.spanier@gmail.com>
2021-11-24 11:41:43 +0100

    Ajout de 2 consoles + 5 jeux dans test

--- Nintendo/src/test/java/test/Test.java
+++ Nintendo/src/test/java/test/Test.java
@@ -1,10 +1,24 @@
 package test;
 
+import metier.Console;
+import metier.Jeu;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Console c1 = new Console("Switch");
+		Console c2 = new Console("GameCube");
+		
+		Jeu j1 = new Jeu("Breath of the Wild", c1);
+		Jeu j2 = new Jeu("Mario Party", c1);
+		Jeu j3 = new Jeu("The Wind Waker", c2);
+		Jeu j4 = new Jeu("Metroid Prime", c2);
+		Jeu j5 = new Jeu("Mario Kart 8", c1);
+		
+		
+		
 	}
 
 }
