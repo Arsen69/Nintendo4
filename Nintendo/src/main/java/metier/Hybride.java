97b1fff2358519f5224fe92e2980482e3c81c4ef
ChloeT <chloetassin@hotmail.com>
2021-11-24 12:17:28 +0100

    Ajout Héritage Console --> Class Portable, Salon, Hybride

---
+++ Nintendo/src/main/java/metier/Hybride.java
@@ -0,0 +1,15 @@
+package metier;
+
+public class Hybride extends Console{
+
+	public Hybride(String nom) {
+		super(nom);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Nintendo/src/main/java/metier/Portable.java
@@ -0,0 +1,16 @@
+package metier;
+
+public class Portable extends Console{
+
+	
+	public Portable(String nom) {
+		super(nom);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
